fb8448289ab24ad6740b6ecc08446f8189dde667
harryli <416880269@qq.com>
2023-07-25 10:00:21 +0400

    add batch

--- kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/proxy/KylinProxyManager.java
+++ kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/proxy/KylinProxyManager.java
@@ -2,5 +2,9 @@ package com.kylin.biz.sdk.proxy;
 
 public class KylinProxyManager {
 
-    public static KylinWalletProxy kylinWalletProxy = new KylinWalletProxyImpl();
+    private static KylinWalletProxy kylinWalletProxy = new KylinWalletProxyImpl();
+
+    public static KylinWalletProxy getKylinWalletProxy() {
+        return kylinWalletProxy;
+    }
 }
