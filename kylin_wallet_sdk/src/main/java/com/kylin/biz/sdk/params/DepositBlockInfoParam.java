04f8d5fbd8508ef0ebe055ce1731bde4d20c001d
phyllaga <phyllaga@gmail.com>
2024-07-19 10:49:26 +0400

    Update DepositBlockInfoParam.java

--- kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/DepositBlockInfoParam.java
+++ kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/DepositBlockInfoParam.java
@@ -43,8 +43,12 @@ public class DepositBlockInfoParam {
 
     private String memo;
 
+    //当前确认数
     private Long confirmations;
 
+    //最大确认数
+    private Long confirmedCount;
+    
     private Date updateTime;
 
     private Date createTime;
