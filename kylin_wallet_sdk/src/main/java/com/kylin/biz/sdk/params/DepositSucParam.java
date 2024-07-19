4b5084110759fd58704c6718d737b0492fee2e40
phyllaga <phyllaga@gmail.com>
2024-07-19 10:00:53 +0400

    添加category

--- kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/DepositSucParam.java
+++ kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/DepositSucParam.java
@@ -30,5 +30,7 @@ public class DepositSucParam {
     private String depositTime;
 
     private String chain;
+    
+    private String paymentCategory;
 
 }
--- kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/WithdrawSucParam.java
+++ kylin_wallet_sdk/src/main/java/com/kylin/biz/sdk/params/WithdrawSucParam.java
@@ -22,4 +22,6 @@ public class WithdrawSucParam {
     private String confirmations;
 //    @ApiModelProperty(value = "提现状态 -1->充提失败 0-> 充提确认  1->提现已提交  2->提现取消  3->充值确认中 4->提现已审核 5->等待确认中 6->系统错误  7->管理员回退")
     private String status;
+    
+    private String paymentCategory;
 }
